5299885214f1d3ca5c7e4f4d24906fe8647e425c
voloferenc <voloferenc@email.com>
2022-02-14 18:56:05 +0100

    git

--- git.sh
+++ git.sh
@@ -1,14 +1,12 @@
 #!/bin/sh
 
-pacman -Sy
-pacman -S --noconfirm git
 echo "Mi legyen a könyvtár pl: /mnt/github vagy /home/user/github, teljes útvonalat adj meg!"
 read directory
 mkdir -p ${directory}
 cd ${directory}
 #cd /home/volo/github
-wget -c https://github.com/voloferenc/stre/blob/main/git.sh
+https://raw.githubusercontent.com/voloferenc/stre/main/git.sh
 chmod +x git.sh
-wget -c https://github.com/voloferenc/stre/blob/main/00base.sh
+https://raw.githubusercontent.com/voloferenc/stre/main/00base.sh
 chmod +x 00base.sh
-./00base.sh
+#./00base.sh
