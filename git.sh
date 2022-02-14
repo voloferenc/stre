a6a8e91170a21cd42b27c529c3e56f24b98be92b
voloferenc <voloferenc@email.com>
2022-02-14 16:05:43 +0100

    git repo letolto

---
+++ git.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+echo "Mi legyen a könyvtár pl: /mnt/github vagy /home/user/github, teljes útvonalat adj meg!"
+read directory
+mkdir -p ${directory}
+cd ${directory}
+#cd /home/volo/github
+git clone https://github.com/voloferenc/stre
