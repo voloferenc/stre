a230e122cc61c2758ed6fea99030325ae3d988eb
voloferenc <voloferenc@email.com>
2022-02-14 12:30:07 +0100

    diskek at lettek nevezve

--- 00base.sh
+++ 00base.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
 #BEGIN Config
 disk1="/dev/nvme0n1" # /dev/vnme0n1
-disk15="/dev/nvme0n1p6" # /boot // disk1 => első lemez, 15 => első lemez ötödik partíció
-disk16="/dev/nvme0n1p7" # /
+bootdev="/dev/nvme0n1p6" # /bootdev // disk1 => első lemez, 15 => első lemez ötödik partíció
+rootdev="/dev/nvme0n1p7" # /
+homedev="/dev/nvme0n1p8" # /home
 disk2="/dev/sda" # /dev/sda
 disk21="/dev/nvme0n1p5" # /dev/sda1 //disk2 => második lemez, 21 => második lemez első partíció
 deditor="nano" # a telepítési lemez alapértelmezett szerkesztője, nem ez lesz a rendszered alapértelmezettje
@@ -20,9 +21,9 @@ then
 fi
 if [ $filesystem = "btrfs" ]
 then
-	mkfs.vfat -F32 $disk15
-	mkfs.btrfs -f $disk16
-	mount $disk16 /mnt
+	mkfs.vfat -F32 $bootdev
+	mkfs.btrfs -f $rootdev
+	mount $rootdev /mnt
 	btrfs subvolume create /mnt/@
 	btrfs subvolume create /mnt/@home
 	btrfs subvolume create /mnt/@var_log
@@ -30,14 +31,14 @@ then
 	btrfs subvolume create /mnt/@swap
 
 	umount /mnt
-	mount -o noatime,compress=zstd,ssd,discard=async,space_cache=v2,subvol=@ $disk16 /mnt
+	mount -o noatime,compress=zstd,ssd,discard=async,space_cache=v2,subvol=@ $rootdev /mnt
 	mkdir -p /mnt/{boot,home,var/log,.snapshots,swap}
-	mount -o noatime,compress=zstd,ssd,discard=async,space_cache=v2,subvol=@home $disk16 /mnt/home
-	mount -o noatime,compress=zstd,ssd,discard=async,space_cache=v2,subvol=@var_log $disk16 /mnt/var/log
-	mount -o noatime,compress=zstd,ssd,discard=async,space_cache=v2,subvol=@snapshots $disk16 /mnt/.snapshots
-	mount -o noatime,compress=zstd,ssd,discard=async,space_cache=v2,subvol=@swap $disk16 /mnt/swap
+	mount -o noatime,compress=zstd,ssd,discard=async,space_cache=v2,subvol=@home $rootdev /mnt/home
+	mount -o noatime,compress=zstd,ssd,discard=async,space_cache=v2,subvol=@var_log $rootdev /mnt/var/log
+	mount -o noatime,compress=zstd,ssd,discard=async,space_cache=v2,subvol=@snapshots $rootdev /mnt/.snapshots
+	mount -o noatime,compress=zstd,ssd,discard=async,space_cache=v2,subvol=@swap $rootdev /mnt/swap
 
-	mount $disk15 /mnt/boot
+	mount $bootdev /mnt/boot
 
 	if [ $disk21 = "nvme0n1p5" ]
 	then
@@ -55,12 +56,12 @@ then
 	mkswap /mnt/swap/swapfile
 	swapon /mnt/swap/swapfile
 else
-	mkfs.vfat -F32 $disk15
-	mkfs.ext4 $disk16
-	mkfs.ext4 $disk17
-	mount $disk16 /mnt
-	mkdir /mnt/boot && mount $disk15 /mnt/boot
-	mkdir /mnt/home && mount $disk17 /mnt/home
+	mkfs.vfat -F32 $bootdev
+	mkfs.ext4 $rootdev
+	mkfs.ext4 $homedev
+	mount $rootdev /mnt
+	mkdir /mnt/boot && mount $bootdev /mnt/boot
+	mkdir /mnt/home && mount $homedev /mnt/home
 	if [ $disk21 = "nvme0n1p5" ]
 	then
 		mkdir /mnt/doksi && mount $disk21 /mnt/doksi
