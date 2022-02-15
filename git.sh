2005965f190f26e303611be5280aa3877a0b7012
voloferenc <voloferenc@gmail.com>
2022-02-15 10:22:10 +0100

    Update git.sh

--- git.sh
+++ git.sh
@@ -10,6 +10,6 @@ cd ${directory}
 #cd /home/volo/github
 #https://raw.githubusercontent.com/voloferenc/stre/main/git.sh
 #chmod +x git.sh
-https://raw.githubusercontent.com/voloferenc/stre/main/00base.sh
+wget -c https://raw.githubusercontent.com/voloferenc/stre/main/00base.sh
 chmod +x 00base.sh
 ./00base.sh
